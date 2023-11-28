f7bccca6b018aace082e4496492437550128819e
Maziar Pedrami <awildroot@gmail.com>
2023-11-28 12:15:19 +0100

    added paths

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const app = express();
 const PORT = 3001;
+const data = require("./data.json");
 
-app.get('/LetsFight', function (req, res){
-  res.send({message: "eudes is cool"})
-})
+app.use(express.json());
 
-app.listen(PORT, ()=>console.log(`server is listening on http://localhost:${PORT}`))
+app.get("/pokemon", function (req, res) {
+  res.send(data);
+});
+
+app.get("/pokemon/:id", (req, res) => {
+  const { id } = req.params;
+  console.log(typeof id)
+  const foundPokemon = data.filter(pokemon => pokemon.id === +id);
+  console.log(foundPokemon);
+  res.json(foundPokemon);
+});
+
+
+app.listen(PORT, () =>
+  console.log(`server is listening on http://localhost:${PORT}`)
+);
