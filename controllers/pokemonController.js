961ee7f2af836380349eb87d329900e58334fe9d
Maziar Pedrami <awildroot@gmail.com>
2023-12-01 14:24:06 +0100

    added /:id/info route

--- controllers/pokemonController.js
+++ controllers/pokemonController.js
@@ -8,14 +8,28 @@ const getPokemon = (req, res) => {
   const { id } = req.params;
   const foundPokemon = data.filter((pokemon) => pokemon.id === +id);
   if (foundPokemon.length > 0) {
-    res.json(foundPokemon);
+    res.json(foundPokemon[0]);
   } else {
     res.status(404).send({ message: "The id is invalid" });
   }
 };
 
 const getPokemonInfo = (req, res) => {
-  res.send("Test");
+  const { id, info } = req.params;
+  const foundPokemon = data.filter((pokemon) => pokemon.id === +id);
+  if (foundPokemon.length === 0) {
+    res.status(404).send({ message: "The id is invalid" });
+  } else {
+    if (info === "type" || info === "base" || info === "name") {
+      const resInfo = foundPokemon[0];
+      res.send(resInfo[info]);
+    } else {
+      res
+        .status(404)
+        .send({ message: "the parameter must be 'type' or 'name' or 'base'" });
+    }
+  }
 };
+
 
 module.exports = { getPokemon, getPokemons, getPokemonInfo };
--- routes/pokemonRoute.js
+++ routes/pokemonRoute.js
@@ -2,18 +2,15 @@
 const express = require("express");
 const router = express.Router();
 const data = require("../data.json");
-const pokemonController = require('../controllers/pokemonController')
+const pokemonController = require("../controllers/pokemonController");
 
 //const { getPokemons, getPokemon, getPokemonInfo} = require('../controllers/pokemonController')
 
-
-
 router.get("/", pokemonController.getPokemons);
-
 
 router.get("/:id", pokemonController.getPokemon);
 
-router.get('/:id/:info', pokemonController.getPokemonInfo  )
+router.get("/:id/:info", pokemonController.getPokemonInfo);
 
 //export default router(ES6);
 module.exports = router;
